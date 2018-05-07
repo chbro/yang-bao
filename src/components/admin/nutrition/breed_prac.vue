49647640702fa7e0f94d890cb973e96e002f289b
yanggreater <915759631@qq.com>
2018-05-07 20:05:47 +0800

    修改用户需求

--- src/components/admin/nutrition/breed_prac.vue
+++ src/components/admin/nutrition/breed_prac.vue
@@ -30,8 +30,8 @@ export default {
             updateBreeding,
             items: [
                 {label: '配种后移至栏/栋', model: 'building'},
-                {label: '母羊商标耳牌', model: 'mEtB', mr: 1},
-                {label: '种公羊商标耳牌', model: 'fEtB'},
+                {label: '母羊商标耳牌', model: 'mEtB'},
+                {label: '种公羊商标耳牌', model: 'fEtB', mr: 1},
                 {label: '配种时间', model: 'breedingT', type: 'time'},
                 {label: '妊娠时间', model: 'gestationT', type: 'time'},
                 {label: '产前免疫种类及时间', model: 'prenatalIT', doubleWidth: true, type: 'time'},
--- src/components/admin/prevention/prevention_list.vue
+++ src/components/admin/prevention/prevention_list.vue
@@ -28,7 +28,6 @@ export default {
                 {label: '商标耳牌号', prop: 'earTag'},
                 {label: '观察检测分析症状',prop: 'symptom'},
                 {label: '诊断结果', prop: 'diagnosisResult'},
-
                 {label: '治疗方案及用药剂量', prop: 'diagnosisMethod'},
                 {label: '总用药量',prop: 'dose'},
                 {label: '治疗效果',prop: 'treatEffect'}
