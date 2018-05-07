2c3c699fc948b770f25f3106487644f924d4d059
JuneLikesYogurt <794899176@qq.com>
2018-05-07 10:24:53 +0800

    comment

---
+++ src/components/admin/comment/accountComment.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="accountComment">
+        <ve-histogram :data="commentChart" width="400px"></ve-histogram>
+    </div>
+</template>
+
+<script>
+import VeHistogram from 'v-charts/lib/histogram'
+
+export default {
+    components: {
+        VeHistogram
+    },
+
+    created () {
+        this.commentChart = {
+            columns: ['类别', '数量'],
+            rows: [
+                {'类别': '非常满意', '数量': '52'},
+                {'类别': '满意', '数量': '97'},
+                {'类别': '一般', '数量': '44'},
+                {'类别': '不满意', '数量': '26'},
+                {'类别': '非常不满意', '数量': '12'}
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="stylus">
+#accountComment
+    width 400px;
+    margin 0 auto;
+</style>
---
+++ src/components/admin/comment/commentResults.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="admin-form">
+        <el-table
+            :data="tableData"
+            style="width: 100%">
+            <el-table-column
+                prop="expertName"
+                label="专家姓名"
+                width="120">
+            </el-table-column>
+            <el-table-column
+                prop="companyName"
+                label="单位">
+            </el-table-column>
+            <el-table-column
+                prop="levelA"
+                label="非常满意/数"
+                width="120">
+            </el-table-column>
+            <el-table-column
+                prop="levelB"
+                label="满意/数"
+                width="120">
+            </el-table-column>
+            <el-table-column
+                prop="levelC"
+                label="一般/数"
+                width="120">
+            </el-table-column>
+            <el-table-column
+                prop="levelD"
+                label="不满意/数"
+                width="120">
+            </el-table-column>
+            <el-table-column
+                prop="levelE"
+                label="非常不满意/数"
+                width="120">
+            </el-table-column>
+        </el-table>
+        <!-- <admin-table
+            :getData="getUsers"
+            :deleteData="deleteUser"
+            :headers.sync="headers">
+        </admin-table> -->
+    </div>
+</template>
+
+<script>
+// import AdminTable from '@/components/admin/table'
+
+export default {
+    // components: {
+    //     AdminTable
+    // },
+    data () {
+        return {
+            expertName: '',
+            companyName: '',
+            levelA: '',
+            levelB: '',
+            levelC: '',
+            levelD: '',
+            levelE: ''
+        }
+    }
+}
+</script>
+
+<style lang="stylus">
+</style>
